7bafcb02f861450ac206570089fd8304108f7fee
Sneha6003 <22bce400@nirmauni.ac.in>
2024-06-12 14:05:52 +0530

    Team Updated

--- components/common/Card.js
+++ components/common/Card.js
@@ -1,36 +1,53 @@
-import Link from "next/link"
-import { TitleSm } from "./Title"
-import { HiOutlineArrowRight } from "react-icons/hi"
+import Link from "next/link";
+import { TitleSm } from "./Title";
+import { HiOutlineArrowRight } from "react-icons/hi";
+import { FaLinkedin, FaGithub, FaUser } from "react-icons/fa";
 
 export const Card = ({ data, caption, show, path }) => {
   return (
-    <>
-      <div className='card'>
-        <div className='card-img'>
-          <img src={data.cover} alt={data.title} />
+    <div className='card'>
+      <div className='card-img'>
+        <img src={data.cover} alt={data.title} />
+      </div>
+      <div className='card-details'>
+        <h1>{data.title}</h1>
+        {caption && <div>{caption}</div>}
+        <div className='flex'>
+          <span>{data.category}</span> {data.date && <span> / {data.date}</span>}
         </div>
-        <div className='card-details'>
-          <Link href={`${path}/${data.id}`} className='title-link'>
-            <TitleSm title={data.title} />
-          </Link>
-          {caption && (
-            <Link href={`${path}/${data.id}`}>
-              {caption} <HiOutlineArrowRight className='link-icon' />
-            </Link>
-          )}
-          <div className='flex'>
-            <span> {data.catgeory} </span> {data.date && <span> / {data.date}</span>}
-          </div>
 
-          {show && (
-            <ul>
-              {data.desc.map((text, i) => (
-                <li key={i}> - {text.text}</li>
-              ))}
-            </ul>
+        {show && (
+          <ul>
+            {data.desc.map((text, i) => (
+              <li key={i}> - {text.text}</li>
+            ))}
+          </ul>
+        )}
+
+        <div className='social-icons'>
+          {data.linkedin && (
+            <a href={data.linkedin} target='_blank' rel='noopener noreferrer'>
+              <div className='icon-container'>
+                <FaLinkedin size={30} />
+              </div>
+            </a>
+          )}
+          {data.github && (
+            <a href={data.github} target='_blank' rel='noopener noreferrer'>
+              <div className='icon-container'>
+                <FaGithub size={30} />
+              </div>
+            </a>
+          )}
+          {data.portfolio && (
+            <a href={data.portfolio} target='_blank' rel='noopener noreferrer'>
+              <div className='icon-container'>
+                <FaUser size={30} />
+              </div>
+            </a>
           )}
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
--- sections/Team.js
+++ sections/Team.js
@@ -1,8 +1,7 @@
-import { teamdata } from "@/assets/data/dummydata"
-import { Card } from "@/components/common/Card"
-import { Title, TitleSm } from "@/components/common/Title"
-import team from "@/pages/team"
-import React from "react"
+import { teamdata } from "@/assets/data/dummydata";
+import { Card } from "@/components/common/Card";
+import { Title, TitleSm } from "@/components/common/Title";
+import React from "react";
 
 const Team = () => {
   return (
@@ -10,19 +9,20 @@ const Team = () => {
       <section className='agency bg-top'>
         <div className='container'>
           <div className='heading-title'>
-            <TitleSm title='MEET OUR CORE TEAM MEMBERS' /> <br />
+            <TitleSm title='MEET OUR TEAM OF SMART AND PASSIONATE MEMBERS' /> <br />
             <br />
-            <Title title='A team of smart & passionate creatives' className='title-bg' />
+            <Title title='CORE TEAM' className='title-bg' />
           </div>
           <div className='grid-4 py'>
             {teamdata.map((item) => (
               <Card data={item} key={item.id} caption={item.post} />
             ))}
           </div>
+          
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
